395bf3646f6ab1eca264569ae0ddc796fa6d302b
Sophia Kaya <me@Me-MacBook-Air.local>
2020-06-08 13:10:14 +0100

    Updates

---
+++ precache-manifest.16559b0289fb55b5ec4919cb698d4aa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b423854fbb4700fed9f7363eb542026d",
+    "url": "/react-crwn-clothing/index.html"
+  },
+  {
+    "revision": "619a8e4f26a1e5243a60",
+    "url": "/react-crwn-clothing/static/css/main.60e28e74.chunk.css"
+  },
+  {
+    "revision": "9feb83bad98cd1026c99",
+    "url": "/react-crwn-clothing/static/js/2.e7627e6c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-crwn-clothing/static/js/2.e7627e6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "619a8e4f26a1e5243a60",
+    "url": "/react-crwn-clothing/static/js/main.a80892fe.chunk.js"
+  },
+  {
+    "revision": "11ee381046b6570c3581",
+    "url": "/react-crwn-clothing/static/js/runtime-main.67fd5baf.js"
+  }
+]);
