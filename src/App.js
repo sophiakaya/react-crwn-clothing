ec119730c93cb3eb125ed8b14e2b3d877b8927a0
Sophia Kaya <me@Me-MacBook-Air.local>
2020-06-12 11:32:00 +0100

    signin page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component.jsx";
+import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header-component";
 
 function App() {
@@ -12,9 +13,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="react-crwn-clothing/" component={HomePage} />
-        <Route exact path="react-crwn-clothing" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
