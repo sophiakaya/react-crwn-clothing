a039c60efef04cc97cfc8fca5e18467a3494805b
Sophia Kaya <me@Me-MacBook-Air.local>
2020-06-14 17:31:34 +0100

    redux added

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -17,6 +17,7 @@ class SignIn extends React.Component {
   }
 
   handleSubmit = async (event) => {
+    event.preventDefault();
     const { email, password } = this.state;
     try {
       await auth.signInWithEmailAndPassword(email, password);
