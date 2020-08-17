c6517ee6097b6f5361403345ab22c44ddf18e0cc
Sophia KAYA <sophiakaya@Sophias-MacBook-Air.local>
2020-08-17 10:32:36 +0100

    routing

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { selectCollection } from "../../redux/shop/shop.selector";
+
+import "./collection.styles.scss";
+
+const CollectionPage = ({ collection }) => {
+  return (
+    <div className="collection-page">
+      <h2>Collection PAGE</h2>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
